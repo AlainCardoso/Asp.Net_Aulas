64790053d6e9524e381afad3f9910c59b3d1ec51
Alain Cardoso <alain_kardoso@hotmail.com>
2017-05-22 11:36:47 -0300

    Trabalhando com o Entity em Asp.Net

---
+++ Aula2205_EF_DF/Aula2205_EF_DF/Views/Categoria/ListaCategorias.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Aula2205_EF_DF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula2205_EF_DF.Views.Categoria
+{
+    public partial class ListaCategorias : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Contexto (Referência para o banco)
+            strConnLojaEntities contexto = new strConnLojaEntities();
+            gvwCategorias.DataSource = contexto.Categoria.ToList();
+            gvwCategorias.DataBind();
+        }
+    }
+}
---
+++ Aula2205_EF_DF/Aula2205_EF_DF/Views/Produto/Produtos.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Produtos.aspx.cs" Inherits="Aula2205_EF_DF.Views.Produto.Produtos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:GridView ID="gvwProdutos" runat="server"></asp:GridView>
+
+</asp:Content>
