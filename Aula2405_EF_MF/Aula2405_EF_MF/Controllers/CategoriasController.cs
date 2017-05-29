86610aa8d14368db2c3bccf70372df15b50b1ed1
Alain Cardoso <alain_kardoso@hotmail.com>
2017-05-29 10:33:57 -0300

    Criando e utilizando a classe controller para Criar, Excluir e utilizar atributo ativo/inativo.

---
+++ Aula2405_EF_MF/Aula2405_EF_MF/Controllers/CategoriasController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Aula2405_EF_MF.Models;
+
+namespace Aula2405_EF_MF.Controllers
+{
+    public class CategoriasController
+    {
+        protected BaseDadosContainer contexto = new BaseDadosContainer();
+        public void Adicionar(Categoria categoria)
+        {
+            if (categoria != null)
+            {
+                contexto.Categorias.Add(categoria);
+                contexto.SaveChanges();
+            }
+        }
+        public List<Categoria> Listar()
+        {
+            // Retorna todas as categorias ativas.
+            return contexto.Categorias.Where(c => c.Ativo == true).OrderBy(c => c.Nome).ToList();
+        }
+        public List<Categoria> ListarInativos()
+        {
+            // Retorna todas as categorias inativos.
+            return contexto.Categorias.Where(c => c.Ativo == false).ToList();
+        }
+        public Categoria BuscarCategoriaPorID(int id)
+        {
+            return contexto.Categorias.Find(id);
+        }
+        // Exclusão física (apaga o registro do banco.
+        //public void Excluir(Categoria categoria)
+        //{
+        //    contexto.Entry(categoria).State = System.Data.Entity.EntityState.Deleted;
+        //    contexto.SaveChanges();
+        //}
+
+        // Exclusão logica (campo Ativo/Inativo).
+        public void Excluir (Categoria categoria)
+        {
+            categoria.Ativo = false;
+
+            contexto.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+        public void Editar(Categoria categoria)
+        {
+            contexto.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+}
---
+++ Aula2405_EF_MF/Aula2405_EF_MF/Views/Categorias/Adicionar.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Adicionar.aspx.cs" Inherits="Aula2405_EF_MF.Views.Categorias.Adicionar" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Adicionar Categoria</h1>
+    <asp:Label ID="lblNome" runat="server" Text="Nome"></asp:Label>
+    <asp:TextBox ID="txtNome" runat="server"></asp:TextBox>
+
+    <asp:Label ID="lblDescricao" runat="server" Text="Descrição"></asp:Label>
+    <asp:TextBox ID="txtDescricao" runat="server" TextMode="MultiLine"></asp:TextBox>
+
+    <asp:Button ID="btnSalvar" runat="server" Text="Salvar" OnClick="btnSalvar_Click" />
+    <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" />
+</asp:Content>
