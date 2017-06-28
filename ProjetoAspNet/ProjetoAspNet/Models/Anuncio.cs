b2371b57d386ed8d1b115514aa7607b706af266d
Alain Cardoso <alain_kardoso@hotmail.com>
2017-06-28 08:14:14 -0300

    Criando o projeto final de asp.net;

---
+++ ProjetoAspNet/ProjetoAspNet/Models/Anuncio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAspNet.Models
+{
+    public class Anuncio
+    {
+        public int AnuncioID { get; set; }
+        public string Produto { get; set; }
+        public string Descrição { get; set; }
+        public string Tipo { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
