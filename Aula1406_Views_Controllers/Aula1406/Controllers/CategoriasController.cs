708a777177fe62202e64ca6995f9b78928e4b997
Alain Cardoso <alain_kardoso@hotmail.com>
2017-07-05 08:14:13 -0300

    Adicionando solução

---
+++ Aula1406_Views_Controllers/Aula1406/Controllers/CategoriasController.cs
@@ -0,0 +1,54 @@
+﻿using Aula1406.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1406.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+            //Retornar a lista de objetos cadastrados
+            return View(categorias);
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult Create(Categoria Categoria, FormCollection campos)
+        {
+            if (ModelState.IsValid)
+            {
+                // insert
+
+            }
+            return View(Categoria);
+        }
+
+        public ActionResult Edit (int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                // update
+            }
+
+            return View(categoria);
+        }
+    }
+}
---
+++ Aula1406_Views_Controllers/Aula1406/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula1406.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage = "Favor preencher o campo Nome!")]
+        [StringLength(15, ErrorMessage = "Você excedeu o limite de 15 caractéres!")]
+        public string Nome { get; set; }
+        [Display(Name = "Descrição"), DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
